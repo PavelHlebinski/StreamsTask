8b27c1eb5c28b96bec8d17dc570498bacea8d43e
PavelKhlebinski <predramer@gamil.com>
2021-04-16 11:59:09 +0300

    Add project files.

---
+++ StreamsTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using StreamsTask.Workers;
+using System;
+
+namespace StreamsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Environment.CurrentDirectory + "\\..\\..\\..\\Files";
+            var text = "some string";
+            var fileName = "Testfile.txt";
+            FileStreamWorker fileStreamWorker = new FileStreamWorker();
+            fileStreamWorker.Write(text, fileName, path);
+            fileStreamWorker.Read(fileName, path);
+        }
+    }
+}
---
+++ StreamsTask/Workers/FileStreamWorker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace StreamsTask.Workers
+{
+    public class FileStreamWorker
+    {
+        public void Write(string text, string fileName, string path)
+        {
+            try
+            {
+                var file = Path.Combine(path, fileName);
+                using var fileStream = new FileStream(file, FileMode.OpenOrCreate, FileAccess.Write);
+                using var streamWriter = new StreamWriter(fileStream);
+                streamWriter.WriteLine(text);
+            }
+            catch
+            {
+                Console.WriteLine("Directory not found");
+            }
+        }
+
+        public void Read(string fileName, string path)
+        {
+            try
+            {
+                var file = Path.Combine(path, fileName);
+                using var fileStream = File.OpenRead(file);
+                using var streamReader = new StreamReader(fileStream);
+                var text = streamReader.ReadToEnd();
+                Console.WriteLine($"Text from file: {text}");
+            }
+            catch
+            {
+                Console.WriteLine("File not Found");
+            }
+        }
+    }
+}
